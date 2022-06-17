b65f72c063a54078e2312d8808492149c5a3865b
HJY <houjingyu@zxkw.com.cn>
2022-06-17 18:40:24 +0800

    web

--- web/server.py
+++ web/server.py
@@ -31,16 +31,18 @@ def show_list():
     mysql_cursor.execute(sql)
     open_data = list()
     not_open_data = list()
+    index = 0
     for info in mysql_cursor.fetchall():
+        index += 1
         now_info = {
-            # 'id': info[0],
+            'id': index,
             'l_id': info[1],
             'uid': info[2],
             'lottery_time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(info[3])),
             'my_cv_id': info[4],
             'reply_date': str(info[5]),
-            'zj_uid': '',
-            'is_me': '否',
+            'zj_uid': info[6],
+            'is_me': '中奖了!!!!!!!!' if info[7] else '否',
         }
         if int(info[3]) > int(time.time()):
             not_open_data.append(now_info)
